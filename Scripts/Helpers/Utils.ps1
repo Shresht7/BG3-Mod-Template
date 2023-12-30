4e7f7ea0fe35f992b16c479d528560ca6511fbdf
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-12-30 17:25:47 +0530

    Create `Get-MetaLSXData` utility function

--- Scripts/Helpers/Utils.ps1
+++ Scripts/Helpers/Utils.ps1
@@ -1,3 +1,6 @@
+# Import Version Helper Functions
+. .\Scripts\Helpers\Version.ps1
+
 <#
 .SYNOPSIS
     Returns the path of the mod's root folder
@@ -19,4 +22,28 @@ function Get-ModRootFolder(
         ".."  # [RootFolder]
     )
     return Resolve-Path (Join-Path @Splat) | Get-Item
+}
+
+<#
+.SYNOPSIS
+    Parses the `meta.lsx` file and returns an XML object
+#>
+function Get-MetaLSXData(
+    # Path to the `meta.lsx` file. If not specified, will try to find the `meta.lsx` file in the workspace
+    [ValidateScript({ (Test-Path -Path $_ -PathType Leaf) && ($_.EndsWith("meta.lsx")) })]
+    [string] $Path = (Get-ChildItem -Path . -File -Recurse -Depth 5 -Filter meta.lsx | Select-Object -First 1 -ExpandProperty FullName)
+) {
+    # Read the `meta.lsx` file
+    $MetaXML = [xml](Get-Content -Path $Path)
+
+    # Return the XMLData object
+    return [PSCustomObject]@{
+        Author      = $MetaXML.SelectSingleNode("//node[@id='ModuleInfo']/attribute[@id='Author']")
+        Name        = $MetaXML.SelectSingleNode("//node[@id='ModuleInfo']/attribute[@id='Name']")
+        Description = $MetaXML.SelectSingleNode("//node[@id='ModuleInfo']/attribute[@id='Description']")
+        Folder      = $MetaXML.SelectSingleNode("//node[@id='ModuleInfo']/attribute[@id='Folder']")
+        UUID        = $MetaXML.SelectSingleNode("//node[@id='ModuleInfo']/attribute[@id='UUID']")
+        Version64   = $MetaXML.SelectSingleNode("//node[@id='ModuleInfo']/attribute[@id='Version64']")
+        Tags        = $MetaXML.SelectSingleNode("//node[@id='ModuleInfo']/attribute[@id='Tags']")
+    }
 }
